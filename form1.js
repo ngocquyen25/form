8bc44dfc3b586326bb418cd3f77aacb7f7c1aebc
ngocquyen25 <lengocquyen2k1@gmail.com>
2023-02-03 23:24:08 +0700

    four commit

---
+++ form1.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var pas1=document.getElementById("password").value;
+    var pas2=document.getElementById("password1").value;                        
+    if(pas1==pas2)
+        alert("Đăng nhập thành công");
+    else
+        alert("Vui lòng đăng nhập lại");
+    
+}
